01215fefee5befafcb600903032bff96f3c51294
PafKlapper <stan@wopwop.cc>
2018-01-17 12:52:03 +0100

    up

--- stage1.sh
+++ stage1.sh
@@ -161,6 +161,9 @@ umount /mnt 2>/dev/null
 }
 trap cleanup INT TERM EXIT
 
+updateGrub
+exit 1
+
 
 # set error file so every 'announce' call below will call the appended scriptures just one time!
 runner=0
